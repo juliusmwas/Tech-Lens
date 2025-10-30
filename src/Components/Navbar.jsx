99a1f99c4d7806385e6b73cf8ac23c2cc6369187
juliusmwass <juliusmwass100@gmail.com>
2025-10-30 20:11:54 +0300

    Updated the navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,23 @@
 import { FaMoon } from "react-icons/fa";
 import { FiSun } from "react-icons/fi";
 import { IoMenu } from "react-icons/io5";
+import { RiCameraLensAiLine } from "react-icons/ri";
 
 export default function Navbar () {
     return(
-        <nav className="flex justify-between p-2 items-center bg-amber-200">
-            <div>
-               <h1>Tech <span>Lens</span></h1>
+        <nav className="flex  justify-between p-3 h-20 shadow-lg items-center bg-white ">
+            <div className="flex items-center gap-2 px-5">
+                <span className="text-blue-700"><RiCameraLensAiLine className="text-3xl" /></span>
+                <h1 className="text-primary font-bold text-3xl">TechLens</h1>
             </div>
-
+ 
             <div>
-                <ul className="flex justify-between items-center gap-5">
-                    <li>Home</li>
-                    <li>Articles</li>
-                    <li>Categories</li>
-                    <li>About</li>
-                    <li>Contact</li>
+                <ul className="flex justify-between  font-normal items-center gap-5">
+                    <li><a href="#" className="hover:font-medium">Home</a></li>
+                    <li><a href="#" className="hover:font-medium">Articles</a></li>
+                    <li><a href="#" className="hover:font-medium">Categories</a></li>
+                    <li><a href="#" className="hover:font-medium">About</a></li>
+                    <li><a href="#" className="hover:font-medium">Contact</a></li>
                 </ul>
             </div>
 
@@ -23,7 +25,8 @@ export default function Navbar () {
                 <FaMoon />
                 <FiSun />
                 <IoMenu className="hidden"/>
-                <button>Get Started</button>
+                <button className="bg-slate-200">Sign in</button>
+                <button className="bg-slate-200">Get Started</button>
             </div>
 
         </nav>
