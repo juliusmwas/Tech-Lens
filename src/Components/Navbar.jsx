c4788b02b56a8215fffa12ca3f52675ef7a1202e
juliusmwass <juliusmwass100@gmail.com>
2025-10-31 13:23:09 +0300

    Updated tha navbar and made it mobile resposive too

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect, useRef } from "react";
-import { IoMenu } from "react-icons/io5";
+import { IoMenu, IoClose } from "react-icons/io5";
 import { RiCameraLensAiLine } from "react-icons/ri";
 import { CiSearch } from "react-icons/ci";
+
 
 export default function Navbar () {
 
@@ -25,48 +26,72 @@ export default function Navbar () {
         if (isFocused) inputRef.current?.focus();
     }, [isFocused]);
 
+    const [toggle, setToggle] = useState (false)
+    const menuToggle = () =>{
+        setToggle(!toggle)
+    }
+
     return(
-        <nav className="flex  justify-between p-3 h-20 shadow-sm items-center bg-gradient-to-r from-slate-200 via-white to-slate-300 text-slate-800 ">
-            <div className="flex items-center gap-2 px-5">
-                <span className="text-blue-700"><RiCameraLensAiLine className="text-3xl" /></span>
-                <h1 className="font-bold text-3xl">TechLens</h1>
-            </div>
- 
-            <div>
-                <ul className="flex justify-between  font-normal text-sm items-center gap-5">
-                    <li><a href="#home" className="hover:font-medium">Home</a></li>
-                    <li><a href="#" className="hover:font-medium">Articles</a></li>
-                    <li><a href="#" className="hover:font-medium">Categories</a></li>
-                    <li><a href="#" className="hover:font-medium">Resources</a></li>
-                    <li><a href="#" className="hover:font-medium">About</a></li>
-                    <li><a href="#" className="hover:font-medium">Contact</a></li>
-                </ul>
-            </div>
+        <div>
+            <nav className="flex  justify-between p-3 h-20 shadow-sm items-center bg-gradient-to-r from-slate-200 via-white to-slate-300 text-slate-800 ">
+                <div className="flex items-center gap-1 lg:gap-2 lg:px-5">
+                    <span className="text-blue-700"><RiCameraLensAiLine className="text-xl lg:text-3xl" /></span>
+                    <h1 className="font-bold text-lg lg:text-3xl">TechLens</h1>
+                </div>
+    
+                <div>
+                    <ul className=" justify-between  font-normal text-sm items-center gap-5 hidden lg:flex">
+                        <li><a href="#home" className="hover:font-medium">Home</a></li>
+                        <li><a href="#" className="hover:font-medium">Articles</a></li>
+                        <li><a href="#" className="hover:font-medium">Categories</a></li>
+                        <li><a href="#" className="hover:font-medium">Resources</a></li>
+                        <li><a href="#" className="hover:font-medium">About</a></li>
+                        <li><a href="#" className="hover:font-medium">Contact</a></li>
+                    </ul>
+                </div>
 
-            <div
-                ref={containerRef}
-                onClick={() => setIsFocused(true)}
-                className={`flex items-center bg-surface border border-gray-400 rounded-lg px-3 py-1 cursor-text transition-all duration-300
-                    ${isFocused ? "w-52 sm:w-72" : "w-28"}
-                `}
-                >
-                <CiSearch className=" text-sm flex-shrink-0" />
-                <input
-                    ref={inputRef}
-                    type="text"
-                    placeholder="Search..."
-                    className="bg-transparent outline-none text-sm  ml-2 w-full"
-                    onFocus={() => setIsFocused(true)}
-                    onBlur={() => setIsFocused(false)}
-                />
-            </div>
-
-            <div className="flex justify-between items-center gap-5">
-                <IoMenu className="hidden"/>
-                <button className=" px-3 py-1 rounded-lg border border-gray-400 font-medium cursor-pointer">Sign in</button>
-                <button className="bg-slate-800 text-slate-100 px-3 py-1 rounded-lg  font-medium cursor-pointer">Get Started</button>
-            </div>
-
-        </nav>
+                <div
+                    ref={containerRef}
+                    onClick={() => setIsFocused(true)}
+                    className={`flex items-center  border border-gray-400 rounded-lg px-3 lg:py-1 cursor-text transition-all duration-300
+                        ${isFocused ? "w-52 sm:w-72" : "w-28"}
+                    `}
+                    >
+                    <CiSearch className=" text-sm flex-shrink-0" />
+                    <input
+                        ref={inputRef}
+                        type="text"
+                        placeholder="Search..."
+                        className="bg-transparent outline-none text-sm  ml-1 w-full"
+                        onFocus={() => setIsFocused(true)}
+                        onBlur={() => setIsFocused(false)}
+                    />
+                </div>
+                
+                <div className="block lg:hidden text-2xl cursor-pointer" onClick={menuToggle}>
+                    {toggle? <IoClose /> :  <IoMenu/> }
+                </div>
+                <div className="hidden lg:flex justify-between items-center gap-5">
+                    <button className=" px-3 py-1 rounded-lg border border-gray-400 font-medium cursor-pointer">Sign in</button>
+                    <button className="bg-slate-800 text-slate-100 px-3 py-1 rounded-lg  font-medium cursor-pointer">Get Started</button>
+                </div>
+            </nav>
+        
+        {/* Mobile menu goes below */}
+        
+        <div className={`w-3/4 grid justify-items-center fixed z-50 right-0 h-full top-20 bg-slate-800 transition-transform duration-300 ${toggle ? "translate-x-0" : "translate-x-full"}`} >
+            <ul className="flex flex-col p-2 text-slate-100  font-normal text-sm items-center gap-5">
+                <li><a href="#home" className="hover:font-medium">Home</a></li>
+                <li><a href="#" className="hover:font-medium">Articles</a></li>
+                <li><a href="#" className="hover:font-medium">Categories</a></li>
+                <li><a href="#" className="hover:font-medium">Resources</a></li>
+                <li><a href="#" className="hover:font-medium">About</a></li>
+                <li><a href="#" className="hover:font-medium">Contact</a></li>
+                <button className=" px-3 py-1  rounded-lg border w-full border-gray-400 font-normal cursor-pointer">Sign in</button>
+                <button className="bg-slate-800 text-slate-100 w-full px-3 py-1 rounded-lg  font-normal cursor-pointer">Get Started</button>
+            </ul>  
+        </div>
+            {toggle && <div className="fixed top-20 left-0 w-full h-full  transition-opacity duration-300 z-40" onClick={() => setToggle(false)}></div>} 
+        </div>
     );
 }
