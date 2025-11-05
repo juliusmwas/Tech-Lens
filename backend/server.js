6a238cf54a9886650c9520a284dfa0f240301079
juliusmwass <juliusmwass100@gmail.com>
2025-11-05 23:09:51 +0300

    Added backed files

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+connectDB(); // connect MongoDB here
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("TechLens backend with MongoDB 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
